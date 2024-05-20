d8d298a16618bea48f7a062be3b83883039732da
Mochi-frontend <hayabusa06mochi@gmail.com>
2024-05-20 12:20:56 +0900

    add Car class content

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -1,4 +1,20 @@
 package com.example.domain;
 
 public class Car {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
